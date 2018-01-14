3da7fe4c85529587edad6d76f97ab3a920caee34
Lyle Waranch <lwaranch520@gmail.com>
2018-01-13 23:21:08 -0800

    UI improvements

--- nwHacks_01/nwHacks_01/ViewController.swift
+++ nwHacks_01/nwHacks_01/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, UITextFieldDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
         // Create a new scene
         let scene = SCNScene()
